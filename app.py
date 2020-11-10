7e31641baa31a316292aa4d9484915333f23c44d
apollo1840 <zoucongyu1993@hotmail.com>
2020-11-10 09:53:42 +0100

    use render

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from datetime import datetime
 import shutil
 
 from utils.gsheet import gs_read_excel
-from data_analysis import plot_the_bar_chart
+from data_analysis import plot_the_bar_chart, min2duration_str
 from constant import *
 
 # in deploy_branch
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def demo():
+def main_page():
     df = gs_read_excel(STUDY_TIME_TABLE_NAME, least_col_name=START_TIME)
 
     # rm the folder to avoid multiple rendering data explode
@@ -27,12 +27,15 @@ def demo():
     new_chart_name = "{}_{}.{}".format(chart_name, datetime.now().strftime('%H_%M_%S'), img_format)
     path_to_chart = os.path.join(os.path.dirname(PATH_TO_BARCHART), new_chart_name)
 
-    plot_the_bar_chart(df, output_path=path_to_chart)
+    df_r = plot_the_bar_chart(df, output_path=path_to_chart)
+    name_winner = list(df_r[NAME])[0]
+    duration_str = min2duration_str(list(df_r[MINUTES])[0])
 
-    return "<img src='{}'>".format(path_to_chart)
+    return render_template('index.html',
+                           path_to_chart=path_to_chart,
+                           name_winner=name_winner,
+                           duration_str=duration_str)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
-
-
--- data_analysis.py
+++ data_analysis.py
@@ -11,11 +11,35 @@ def duration_in_mins(t_start, t_end):
     return (t_end - t_start).seconds / 60
 
 
+def min2duration_str(minutes):
+    duration_str = ""
+    hour = int(minutes//60)
+    min = int(minutes % 60)
+
+    if hour != 0:
+        if hour == 1:
+            duration_str += "1 hour"
+        else:
+            duration_str += "{} hours".format(hour)
+
+    if min != 0:
+        duration_str += " {} minutes".format(min)
+
+    return duration_str
+
+
 def plot_the_bar_chart(df, output_path="sample.png"):
     df[MINUTES] = [duration_in_mins(t_start, t_end) for t_start, t_end in zip(df[START_TIME], df[END_TIME])]
     df_r = df.groupby(NAME)[MINUTES].apply(sum)
     df_r = df_r.reset_index()
+    df_r = df_r.sort_values(by=MINUTES, ascending=False)
 
+    sns.set_style("darkgrid")
     fig = plt.figure(figsize=(10, 10))
     sns.barplot(data=df_r, y=df_r[NAME], x=df_r[MINUTES])
+    plt.title("The study time of the candidates (in minutes)")
     fig.savefig(output_path)
+
+    return df_r
+
+
