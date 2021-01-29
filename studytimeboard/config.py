b935ae329d04aa7356d0c05b9bd84424c7964478
apollo1840 <zoucongyu1993@hotmail.com>
2021-01-29 18:01:47 +0100

    debug

--- studytimeboard/config.py
+++ studytimeboard/config.py
@@ -1,9 +1,24 @@
 from .constant import SHEET_EVENTS, SHEET_EVENTS_DEBUG, SHEET_USERBANK, SHEET_USERBANK_DEBUG
 
 debug_mode = True
-main_googlesheet_name = SHEET_EVENTS_DEBUG
-user_googlesheet_name = SHEET_USERBANK_DEBUG
-add_examples = False
-add_users = False
+
+if debug_mode:
+    has_gs_auth = False
+    if has_gs_auth:
+        main_googlesheet_name = SHEET_EVENTS_DEBUG
+        user_googlesheet_name = SHEET_USERBANK_DEBUG
+        add_examples = False
+        add_users = False
+    else:
+        main_googlesheet_name = None
+        user_googlesheet_name = None
+        add_examples = True
+        add_users = True
+else:
+    main_googlesheet_name = SHEET_EVENTS
+    user_googlesheet_name = SHEET_USERBANK
+    add_examples = False
+    add_users = False
+
 
 
