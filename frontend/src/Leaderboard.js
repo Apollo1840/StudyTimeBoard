603b15a83db2326fb552643453aed7f70ab9aad1
apollo1840 <zoucongyu1993@hotmail.com>
2021-03-16 23:01:28 +0100

    barchart enable

--- frontend/src/Leaderboard.js
+++ frontend/src/Leaderboard.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React from "react";
+import BarchartMinutesPerPersonPerWeekday from "./components/charts/barchart_minutes_per_person";
 
 class Leaderboard extends React.Component {
+  state = {
+    path_to_chart_lastweek: null,
+    path_to_chart_all: null,
+    name_winner_lastweek: null,
+    duration_str_lastweek: null,
+    name_winner: null,
+    duration_str: null,
+  };
 
-    state = {
-        "path_to_chart_lastweek": null,
-        "path_to_chart_all": null,
-        "name_winner_lastweek": null,
-        "duration_str_lastweek": null,
-        "name_winner": null,
-        "duration_str": null
-    }
-  
-    componentDidMount() {
-        fetch("/api/get_leaderboards")
-        .then(response => response.json()
-            .then(jsondata => console.log(jsondata)))
-      }
-  
-    render(){
-        return(
-            <div>leaderboards</div>
-        )
-    }
+  componentDidMount() {
+    fetch("/api/get_leaderboards").then((response) =>
+      response.json().then((jsondata) => this.setState(jsondata))
+    );
   }
-  
-  export default Leaderboard;
+
+  render() {
+    return (
+      <div>
+        <BarchartMinutesPerPersonPerWeekday
+          title="Leaderboard of the last week"
+          users={["congyu", "shangsu", "diqing"]}
+          data={[
+            [10, 2, 3],
+            [1, 0, 3],
+            [23, 12, 0],
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+export default Leaderboard;
