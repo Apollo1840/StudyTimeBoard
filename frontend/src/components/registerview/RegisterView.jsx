7e45f954de6800a2bceb23b659a71e45607c0129
Diqing Chang <diqing.chang@tum.de>
2021-04-04 18:40:53 +0200

    all files with indent=2*spaces

--- frontend/src/components/registerview/RegisterView.jsx
+++ frontend/src/components/registerview/RegisterView.jsx
@@ -5,50 +5,51 @@ import {login} from "../../redux/authActions";
 import store from "../../redux-store";
 
 class RegisterView extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
     }
 
-    handleSubmit(event) {
-        event.preventDefault()
-        let username= this.state.username
-        let password = this.state.password
 
-        AuthService.register(username, password)
-        .then((data) => {
-            store.dispatch(login(username,data.token));
-        })
-        .catch((e) => {
-            alert(e);
-            this.setState({
-                error: e
-            });
-        });
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    render() {
-        return(
-            <main className="container">
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="formUsername">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" onChange={e => this.setState({username: e.target.value})}/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter password" onChange={e => this.setState({password: e.target.value})}/>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">Submit</Button>
-                </Form>
-            </main>
-        );
-    }
+  handleSubmit(event) {
+    event.preventDefault()
+    let username= this.state.username
+    let password = this.state.password
+
+    AuthService.register(username, password)
+    .then((data) => {
+      store.dispatch(login(username,data.token));
+    })
+    .catch((e) => {
+      alert(e);
+      this.setState({
+        error: e
+      });
+    });
+  }
+
+  render() {
+    return(
+      <main className="container">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" onChange={e => this.setState({username: e.target.value})}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" onChange={e => this.setState({password: e.target.value})}/>
+          </Form.Group>
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
+      </main>
+    );
+  }
 }
 
 export default RegisterView;
