d94b4d195966c5d056996a8cb442224150919caa
apollo1840 <zoucongyu1993@hotmail.com>
2021-04-16 13:51:42 +0200

    deployed backend

--- frontend/src/shared/serverUrls.js
+++ frontend/src/shared/serverUrls.js
@@ -1,6 +1,7 @@
 export const SERVER_BASE_URL = "http://0.0.0.0:5555";
+//export const SERVER_BASE_URL = "https://studytimeboard-backend.herokuapp.com";
 export const REGISTRATION_URL = "/api/registration";
 export const LOGIN_URL = "/api/login";
 export const LOGOUT_URL = "/api/logout";
 export const MINUTES_LASTWEEK_URL = "/api/minutes_lastweek";
-export const MINUTES_TOTAL= "/api/minutes_total";
+export const MINUTES_TOTAL = "/api/minutes_total";
