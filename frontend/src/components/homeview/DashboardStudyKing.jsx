0c00af2c1c67b3be98b788a7a60d9a59ad85bf30
apollo1840 <zoucongyu1993@hotmail.com>
2021-04-16 11:30:27 +0200

    debug

--- frontend/src/components/homeview/DashboardStudyKing.jsx
+++ frontend/src/components/homeview/DashboardStudyKing.jsx
@@ -17,6 +17,7 @@ class DashboardStudyKing extends Component {
   };
 
   componentDidMount() {
+    //todo: handle error properly
     StudyKingService.getStudyKing()
       .then((data) =>
         this.setState({
