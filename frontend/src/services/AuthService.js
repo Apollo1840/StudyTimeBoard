9bacf4c58b1b68891faad67dd71959a9a591fa14
Diqing Chang <diqing.chang@tum.de>
2021-04-26 16:35:43 +0200

    window.replace instead of this.props.history.push

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,76 +1,85 @@
 import HttpService from "./HttpService";
 import store from "../redux-store";
-import {logout} from "../redux/authActions";
-import {SERVER_BASE_URL, REGISTRATION_URL, LOGIN_URL, LOGOUT_URL} from "../shared/serverUrls.js";
+import { logout } from "../redux/authActions";
+import {
+  SERVER_BASE_URL,
+  REGISTRATION_URL,
+  LOGIN_URL,
+  LOGOUT_URL,
+} from "../shared/serverUrls.js";
 
 export default class AuthService {
-
-    static register(user,pass, userData) {
-      return new Promise((resolve,reject) => {
-          HttpService.post(SERVER_BASE_URL + REGISTRATION_URL, {
-              username: user,
-              password: pass,
-              userData: userData
-          }, (data) => {
-              resolve(data);
-              this.props.history.push('/');  // redirect to homepage
-          }, (errorMsg) => {
-              reject(errorMsg);
-          });
-      });
-    }
-
-    static login(user, pass) {
-      return new Promise((resolve, reject) => {
-        HttpService.post(
-          SERVER_BASE_URL + LOGIN_URL,
-          {
-            username: user,
-            password: pass,
-          },
-          (data) => {
-            resolve(data);
-            this.props.history.push('/');  // redirect to homepage
-          },
-          (errorMsg) => {
-            reject(errorMsg);
-          }
-        );
-      });
-    }
-
-    static logout() {
-      store.dispatch(logout());
-      return new Promise((resolve, reject) => {
-        HttpService.post(
-          SERVER_BASE_URL + LOGOUT_URL,
-          (data) => {
-            window.location.assign(AuthService.frontEndURL());
-            resolve(data);
-          },
-          (errorMsg) => {
-            reject(errorMsg);
-          }
-        );
-      });
-    }
-
-    static getCurrentUser() {
-      let token = store.getState().auth.token;
-      if (!token || token === "null" || token === null) return {};
-
-      let base64Url = token.split(".")[1];
-      let base64 = base64Url.replace("-", "+").replace("_", "/");
-      return {
-        id: JSON.parse(window.atob(base64)).id,
-        username: JSON.parse(window.atob(base64)).username,
-      };
-    }
-
-    static isAuthenticated() {
-      return (
-        store.getState().auth.token !== null &&
-        store.getState().auth.token !== "null"
+  static register(user, pass, userData) {
+    return new Promise((resolve, reject) => {
+      HttpService.post(
+        SERVER_BASE_URL + REGISTRATION_URL,
+        {
+          username: user,
+          password: pass,
+          userData: userData,
+        },
+        (data) => {
+          window.location.replace("/");
+          resolve(data);
+        },
+        (errorMsg) => {
+          reject(errorMsg);
+        }
       );
-    }
+    });
   }
+
+  static login(user, pass) {
+    return new Promise((resolve, reject) => {
+      HttpService.post(
+        SERVER_BASE_URL + LOGIN_URL,
+        {
+          username: user,
+          password: pass,
+        },
+        (data) => {
+          window.location.replace("/");
+          resolve(data);
+        },
+        (errorMsg) => {
+          reject(errorMsg);
+        }
+      );
+    });
+  }
+
+  static logout() {
+    store.dispatch(logout());
+    return new Promise((resolve, reject) => {
+      HttpService.post(
+        SERVER_BASE_URL + LOGOUT_URL,
+        (data) => {
+          window.location.replace("/");
+          resolve(data);
+        },
+        (errorMsg) => {
+          reject(errorMsg);
+        }
+      );
+    });
+  }
+
+  static getCurrentUser() {
+    let token = store.getState().auth.token;
+    if (!token || token === "null" || token === null) return {};
+
+    let base64Url = token.split(".")[1];
+    let base64 = base64Url.replace("-", "+").replace("_", "/");
+    return {
+      id: JSON.parse(window.atob(base64)).id,
+      username: JSON.parse(window.atob(base64)).username,
+    };
+  }
+
+  static isAuthenticated() {
+    return (
+      store.getState().auth.token !== null &&
+      store.getState().auth.token !== "null"
+    );
+  }
+}
