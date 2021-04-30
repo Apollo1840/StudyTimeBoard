44076cb8e18b7eb26862b01272c244c48eaac573
apollo1840 <zoucongyu1993@hotmail.com>
2021-04-30 17:41:06 +0200

    revert sk change

--- frontend/src/components/homeview/homeview_components/DashboardStudyKing.jsx
+++ frontend/src/components/homeview/homeview_components/DashboardStudyKing.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { LineChartIntervalSingleDay } from "../../shared/charts/LineChartInterval";
 import StudyKingService from "../../../services/StudyKingService";
-
 class DashboardStudyKing extends Component {
   state = {
     winner: "some user",
@@ -23,28 +22,14 @@ class DashboardStudyKing extends Component {
   componentDidMount() {
     //todo: handle error properly
     StudyKingService.getStudyKing()
-      .then((data) => {
-        this.setState(data);
-        console.log(this.state);
-      })
+      .then((data) => this.setState(data))
       .catch((msg) => {
         console.error(msg);
         alert(msg);
       });
   }
 
-  transformTimeLine = (timeline) => {
-    // input:  array of arrays of strs, each array of strs contains 2 str, stands for : startTIme, endTime
-    // output: array of arrays of Dates, each array of Dates contains three Date, stands for : Date, startTime, EndTime
-    return timeline.map((strTimeArray) => [
-      new Date("2000.1.1"),
-      new Date("2000.1.1 ".concat(strTimeArray[0])),
-      new Date("2000.1.1 ".concat(strTimeArray[1])),
-    ]);
-  };
-
   render() {
-    console.log(this.transformTimeLine(this.state.timeline));
     return (
       <div id="today_study_king_display" className="mt-5 mb-5">
         <h3 style={{ fontSize: "150%" }}>Today's study king:</h3>
@@ -53,9 +38,7 @@ class DashboardStudyKing extends Component {
           <b> 🔥 {this.state.winner} </b> (with {this.state.winnerMinutes})
         </p>
         <div className="text-center">
-          <LineChartIntervalSingleDay
-            data={this.transformTimeLine(this.state.timeline)}
-          />
+          <LineChartIntervalSingleDay data={this.state.timeline} />
         </div>
       </div>
     );
