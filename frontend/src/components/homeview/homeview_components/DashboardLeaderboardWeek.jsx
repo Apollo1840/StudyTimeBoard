e9514dc2ced7e34aab6a8f0ddb9297d80031524e
apollo1840 <zoucongyu1993@hotmail.com>
2021-05-02 13:25:05 +0200

    with winner and minutes

--- frontend/src/components/homeview/homeview_components/DashboardLeaderboardWeek.jsx
+++ frontend/src/components/homeview/homeview_components/DashboardLeaderboardWeek.jsx
@@ -4,22 +4,43 @@ import TimeboardService from "../../../services/TimeboardService";
 import { CURRENT } from "../../../services/TimeboardService";
 class DashboardLeaderboardWeek extends Component {
   state = {
-    data: [], // similar to weeklyData in LeaderboardView
-    winner: "someone",
-    winnerTime: "sometime",
+    weeklyData: [], // similar to weeklyData in LeaderboardView
+    weeklyWinner: "someone",
+    weeklyWinnerMinutes: 0,
   };
 
   componentDidMount() {
     TimeboardService.getMinutesLastWeek(CURRENT)
-      .then((data) => this.setState({ data: data }))
+      .then((data) => {
+        // TODO: check js object data structure
+        let leaderboard = {}; // weekly leaderboard
+        Object.keys(data).forEach((weekday) => {
+          Object.keys(data[weekday]).forEach((name) => {
+            if (!leaderboard[name]) leaderboard[name] = 0;
+            leaderboard[name] += data[weekday][name];
+          });
+        });
+        let weeklyWinner = this.getKeyWithMaxValue(leaderboard);
+        this.setState({
+          weeklyData: data,
+          weeklyWinner: weeklyWinner,
+          weeklyWinnerMinutes: leaderboard[weeklyWinner],
+        });
+      })
       .catch((msg) => {
         console.error(msg);
         alert(msg);
       });
   }
 
+  // get the key with max value, json object must contain multiple key value pairs
+  getKeyWithMaxValue = (jsonObject) => {
+    return Object.keys(jsonObject).reduce((a, b) =>
+      jsonObject[a] > jsonObject[b] ? a : b
+    );
+  };
+
   render() {
-    console.log(this.state.data);
     return (
       <div id="lastweek_leaderboard_display" className="mt-5 mb-5">
         <h3 style={{ fontSize: "150%" }}> Leaderboard of this week: </h3>
@@ -27,13 +48,13 @@ class DashboardLeaderboardWeek extends Component {
 
         <div>
           <p style={{ fontSize: "120%" }}>
-            🏆 The leader of this board is:<b> {this.state.winner} </b> (with{" "}
-            {this.state.winnerTime})
+            🏆 The leader of this board is:<b> {this.state.weeklyWinner} </b>{" "}
+            (with {this.state.weeklyWinnerMinutes} minutes)
           </p>
         </div>
 
         <div className="text-center">
-          <BarChartMinutesPerPersonPerToday data={this.state.data} />
+          <BarChartMinutesPerPersonPerToday data={this.state.weeklyData} />
         </div>
       </div>
     );
