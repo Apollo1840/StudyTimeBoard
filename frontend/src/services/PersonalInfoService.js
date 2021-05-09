acc2a3b00d8cfb95cdc2d28d82e093fa6bf7354e
apollo1840 <zoucongyu1993@hotmail.com>
2021-05-09 16:04:07 +0200

    debug

--- frontend/src/services/PersonalInfoService.js
+++ frontend/src/services/PersonalInfoService.js
@@ -1,12 +1,15 @@
 import HttpService from "./HttpService";
-import { SERVER_BASE_URL, NUM_STARS_URL } from "../shared/serverUrls.js";
+import {
+  SERVER_BASE_URL,
+  PERSONAL_NUM_STARS_URL,
+} from "../shared/serverUrls.js";
 
-export default class c {
+export default class PersonalinfoService {
   // Get logged minutes of all users of last week
   static getNumberStars() {
     return new Promise((resolve, reject) => {
       HttpService.get(
-        SERVER_BASE_URL + NUM_STARS_URL,
+        SERVER_BASE_URL + PERSONAL_NUM_STARS_URL,
         (data) => {
           resolve(data);
         },
