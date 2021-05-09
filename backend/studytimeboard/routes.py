77bf9e4c77f8c43ac116d4531f99ff7a477047be
apollo1840 <zoucongyu1993@hotmail.com>
2021-05-09 17:08:27 +0200

    remove all rendering func of flask

--- backend/studytimeboard/routes.py
+++ backend/studytimeboard/routes.py
@@ -233,111 +233,37 @@ def api_logout():
     return {"status": "success", "data": {"token": None}}, 200
 
 
-@app.route('/api/admin/clean_chart_folder', methods=['GET'])
-def api_admin_clean_chart_folder():
-    clean_chart_folder()
-
-# admin pages
+# admin APIs
 @app.route('/admin_log')
 def admin_log():
     with open(logger.filename, "r") as f:
         infos = f.readlines()
     for info in infos:
-        if len(info.strip()) > 0:
-            flash(info, "success")
-    return render_template('about.html')
-
+        print(info)
 
 @app.route('/admin_reload_data')
 def admin_reload_data():
     logger.info("ADMIN: admin_reload_data")
     dbapi.init_db()
-    return render_template('about.html')
-
 
 @app.route('/admin_clean_data')
 def admin_clean_data():
     dbapi.init_empty()
-    return render_template('about.html')
-
-
+  
 @app.route('/admin_create_some_data')
 def admin_create_some_data():
     dbapi.into_some_examples()
-    return render_template('about.html')
-
 
 @app.route('/admin_create_some_users')
 def admin_create_some_user():
     dbapi.into_some_users()
-    return render_template('about.html')
-
 
 @app.route('/admin_star', methods=['GET', 'POST'])
 def admin_star():
     if USERNAME in request.args:
         username = request.args[USERNAME]
         dbapi.into_user_onestar(username)
-    return render_template('about.html')
 
-# depracated
-@app.route('/', methods=["GET", "POST"])
-@app.route("/home", methods=["GET", "POST"])
-def home():
-    if current_user.is_authenticated:
-        username = current_user.username
-    else:
-        username = SOMEONE
-
-    # 0. handle the time input
-    if request.method == "POST":
-        if username == SOMEONE:  # user not authenticated
-            username = request.form.get("username")
-
-        if username in dbapi.all_users():
-            result_signal = dbapi.into_from_request(request, username)
-        else:
-            flash(FlashMessages.NO_SUCH_USER, "danger")
-            result_signal = True
-
-        if not result_signal:
-            flash(FlashMessages.WRONG_DURATION, "danger")
-
-    # if the user type in the form with duration, the next user is unknown, so the username is SOMEONE
-    if not current_user.is_authenticated:
-        username = SOMEONE
-
-    df_all = get_df_ana(dbapi)
-
-    studying_users = info_studying_users(df_all)
-    no_studying_users = len(studying_users) == 0
-
-    # 1. user previous status
-    user_status, user_status_time = info_user_status(df_all, username)
-
-    # 2. other chart info
-    df_last_week = to_this_week_table(df_all)
-
-    # prepare to plot
-    # first rm the folder to avoid image database exploding
+@app.route('/api/admin/clean_chart_folder', methods=['GET'])
+def api_admin_clean_chart_folder():
     clean_chart_folder()
-
-    # 2.1 today's study king
-    today_king, duration_str_king, path_to_chart_king = info_today_study_king(df_last_week)
-
-    # 2.2 show the last week chart:
-    # get display information
-    name_winner_lw, duration_str_lw, path_to_chart_lw = info_minutes_dashboard(df_last_week, chart_prefix="lastweek",
-                                                                               sep=TODAY_OR_NOT)
-
-    return render_template('home.html',
-                           studying_users=studying_users,
-                           no_studying_users=no_studying_users,
-                           user_status=user_status,
-                           user_status_time=user_status_time,
-                           path_to_chart_king=path_to_chart_king,
-                           today_king=today_king,
-                           duration_str_king=duration_str_king,
-                           path_to_chart_lastweek=path_to_chart_lw,
-                           name_winner=name_winner_lw,
-                           duration_str_winner=duration_str_lw)
